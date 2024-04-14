d0ef4d7656db29bd8fa9f08799b47b903374b234
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-04-14 10:33:46 +0200

    Add files via upload

---
+++ Program Files/launch_options/Info.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+
+
+path = os.path.join('Program Files', 'main.py')
+launch_arguments = 'info'
+command: str = f'python "{path}" -{launch_arguments}'
+
+
+
+def main():
+    try:
+        subprocess.run(command)
+    except Exception as e:
+        print(f'An unexpected {type(e).__name__} occured: {str(e)}')
+        input()
+
+if __name__ == '__main__':
+    main()
