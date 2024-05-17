81561797037ee428e291301c7124c535d7f5f19e
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-05-17 22:53:32 +0200

    0.8.2

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,5 @@
-const website_version = "website version: 0.8.1"
-const min_width = 1920
+const website_version = "website version: 0.8.2"
+const min_width = 1610
 
 window.onload = function load() {
     const website_version_container = document.getElementById('website-version-container');
