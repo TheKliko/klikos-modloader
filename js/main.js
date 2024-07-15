2e27153e7f0ee382266d82284998a30425b8d472
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-07-15 23:33:57 +0200

    v2.0.0

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const website_version = '2.0.0';
+
+// window.onload = function onload() {
+document.addEventListener("DOMContentLoaded", function() {
+    var version_container = document.getElementById('website-version')
+    version_container.innerHTML = website_version
+
+    set_darkmode();
+});
+
+function set_darkmode() {
+    var darkmode = localStorage.getItem('darkmode');
+    var body = document.getElementsByTagName('body')[0];
+
+    if (darkmode === 'true') {
+        body.classList.add('darkmode');
+    } else {
+        body.classList.remove('darkmode');
+    };
+};
+
+function toggle_darkmode() {
+    var darkmode = localStorage.getItem('darkmode');
+
+    if (darkmode === 'true') {
+        localStorage.setItem('darkmode', 'false');
+    } else {
+        localStorage.setItem('darkmode', 'true');
+    };
+
+    set_darkmode();
+};
+
+function hamburger_menu_toggle() {
+    if (mobile_device()) {
+        var navigation = document.getElementById('header-navigation');
+        var hamburger = document.getElementById('header-hamburger');
+        navigation.classList.toggle('active')
+        hamburger.classList.toggle('active')
+    };
+};
+
+function download_now() {
+    if (mobile_device()) {
+        alert('Download not available for mobile devices');
+        return;
+    };
+    window.open('download.html');
+};
+
+function mobile_device() {
+    return window.innerWidth <= 768;
+};
