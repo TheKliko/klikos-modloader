3aeefab054629067d8d479004d88455de80a6591
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-09-11 17:14:24 +0200

    Delete Program Files/modules/interface directory

--- Program Files/modules/interface/bullet.py
+++
@@ -1,4 +0,0 @@
-class Bullet:
-    POINT: str = '\u2022'
-    TRIANGLE: str = '\u2023'
-    SQUARE: str = '\u25AA'
--- Program Files/modules/interface/title.py
+++
@@ -1,5 +0,0 @@
-from os import system
-
-
-def title(title: str = "py.exe") -> None:
-    system(f'title {title}')
