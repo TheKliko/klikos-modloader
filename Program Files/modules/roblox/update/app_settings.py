0d12f1415597c982ddc13cd2192fff8286ee3d83
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-09-11 17:18:00 +0200

    Delete Program Files/modules/roblox directory

--- Program Files/modules/roblox/update/app_settings.py
+++
@@ -1,7 +0,0 @@
-APP_SETTINGS: str = '\n'.join([
-    '<?xml version="1.0" encoding="UTF-8"?>',
-    '<Settings>',
-    '\t<ContentFolder>content</ContentFolder>',
-    '\t<BaseUrl>http://www.roblox.com</BaseUrl>',
-    '</Settings>'
-])
--- Program Files/modules/roblox/update/version/__init__.py
+++
@@ -1,4 +0,0 @@
-from .latest import latest
-from .is_installed import is_installed, is_installed_localappdata
-from .exceptions import *
-from .manifest import get_manifest
