8d2b70be6f51039823fddfda671032c93d0d67d9
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-09-28 23:29:45 +0200

    v1.5.0

---
+++ pyinstaller/main.py
@@ -0,0 +1,39 @@
+from os import path, name, system
+from subprocess import run
+from sys import executable, argv, exit
+
+
+COLOR_RESET: str = "\033[0m"
+COLOR_ERROR: str = "\033[38;2;255;107;104m"
+COLOR_WARNING: str = "\033[38;2;241;176;12m"
+
+
+def main():
+    try:
+        filepath = path.join(path.dirname(executable), 'Program Files', 'main.py')
+
+        command = ["python", filepath] + argv[1:]
+        returncode = run(command).returncode
+
+        if returncode == 9009:
+            python_not_found()
+
+
+    except Exception as e:
+        system('cls' if name == 'nt' else 'clear')
+        print(COLOR_ERROR+"ERROR: "+type(e).__name__)
+        print(COLOR_WARNING+str(e))
+        print(COLOR_RESET)
+        input("Press ENTER to exit . . .")
+
+
+def python_not_found() -> None:
+    system('cls' if name == 'nt' else 'clear')
+    print(COLOR_ERROR+"ERROR: Python was not found!")
+    print(COLOR_WARNING+"Please (re)install Python, make sure to check the box that says: \"Add python.exe to PATH\"")
+    print(COLOR_RESET)
+    input("Press ENTER to exit . . .")
+
+
+if __name__ == "__main__":
+    main()
