dd2b89774aaf62f8eeeb35b1eda14f9d49db572d
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-10-01 17:34:48 +0200

    v1.5.0

---
+++ Program Files/modules/menu/fonts.py
@@ -0,0 +1,9 @@
+from tkinter.font import Font
+
+from customtkinter import CTkFont
+
+text_font: CTkFont = CTkFont(family="Segoe UI", size=12)
+title_font: CTkFont = CTkFont(family="Segoe UI", size=20)
+subtitle_font: CTkFont = CTkFont(family="Segoe UI", size=16)
+
+navbar_font: CTkFont = CTkFont(family="Segoe UI", size=12)
