2365d901d2e7dd52fb79cf6c94c888f7f0cdd9a3
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-10-11 17:44:42 +0200

    Update api.py

--- Program Files/modules/request/api.py
+++ Program Files/modules/request/api.py
@@ -1,7 +1,7 @@
 class Api:
     @staticmethod
     def latest_version() -> str:
-        return r"https://raw.githubusercontent.com/TheKliko/klikos-modloader/refs/heads/version-1.5.0/GitHub%20Files/version.json"
+        return r"https://raw.githubusercontent.com/TheKliko/klikos-modloader/GitHub%20Files/version.json"
     
 
     @staticmethod
