1b6b5068090aba8943f01390314eb245e2830009
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-11-10 17:00:59 +0100

    Delete modules/modules directory

--- modules/modules/functions/process_exists.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-# https://stackoverflow.com/a/29275361
-def process_exists(process: str) -> bool:
-    call = 'TASKLIST', '/FI', 'imagename eq %s' % process
-    output = subprocess.check_output(call, creationflags=subprocess.CREATE_NO_WINDOW).decode()
-    last_line = output.strip().split('\r\n')[-1]
-    return last_line.lower().startswith(process.lower())
