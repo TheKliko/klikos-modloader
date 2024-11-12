ed78f90ce4fb870714625c43d8110ab66e2d7e28
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-11-12 18:02:50 +0100

    Add files via upload

--- modules/functions/process_exists.py
+++ modules/functions/process_exists.py
@@ -1,9 +1,15 @@
-import subprocess
-
-
 # https://stackoverflow.com/a/29275361
+# import subprocess
+# def process_exists(process: str) -> bool:
+#     call = 'TASKLIST', '/FI', 'imagename eq %s' % process
+#     output = subprocess.check_output(call, creationflags=subprocess.CREATE_NO_WINDOW).decode()
+#     last_line = output.strip().split('\r\n')[-1]
+#     return last_line.lower().startswith(process.lower())
+
+
+# New method to prevent UnicodeDecodeError
+import psutil
 def process_exists(process: str) -> bool:
-    call = 'TASKLIST', '/FI', 'imagename eq %s' % process
-    output = subprocess.check_output(call, creationflags=subprocess.CREATE_NO_WINDOW).decode()
-    last_line = output.strip().split('\r\n')[-1]
-    return last_line.lower().startswith(process.lower())
+    processes = list(p.name() for p in psutil.process_iter())
+    count = processes.count(process)
+    return count >= 1
