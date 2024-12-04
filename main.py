6ecc4f0691c16a690eff3a70f0d2480845eff51c
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-12-04 23:01:01 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from modules import Logger
-from modules import startup
+import sys
+
+from modules import Logger  # Imported first to initialize the logger
+from modules import LaunchMode, exception_handler, startup
 
 
 def main() -> None:
+    if getattr(sys, "frozen", False):
+        import pyi_splash
+        if pyi_splash.is_alive():
+            pyi_splash.close()
+
     try:
-        # startup.run()
-        pass
+        startup.run()
+        
+        match LaunchMode.get().lower():
+            case "menu":
+                input("Menu")
+            case "player":
+                input("Player")
+            case "studio":
+                input("Studio")
+            case "rpc":
+                input("RPC")
 
     except Exception as e:
-        pass
+        exception_handler.run(e)
+    
+    finally:
+        Logger.info("Shutting down...")
 
 
 if __name__ == "__main__":
