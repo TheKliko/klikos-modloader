8d7c93da478c9f73a46f519c85692968a8ee89d9
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-01-01 17:12:34 +0100

    Delete modules/functions directory
    pushed to the wrong branch :(

--- modules/functions/roblox/user_channel.py
+++
@@ -1,8 +0,0 @@
-from modules import request
-from modules.request import Api, Response
-
-
-def get(binaryType: str) -> str:
-    response: Response = request.get(Api.Roblox.Deployment.channel(binaryType), cached=True)
-    data: dict = response.json()
-    return data["channelName"]
