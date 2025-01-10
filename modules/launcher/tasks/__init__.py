f5ef7f22d7131566c56e330540173a74eea831ed
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-01-10 18:10:34 +0100

    Update launcher
    Make restoring default files optional,
    send more updates to textvariable,
    move the logic for forced reinstallations from restore_default_files.py to __init__.py,
    add more comments

--- modules/launcher/tasks/__init__.py
+++ modules/launcher/tasks/__init__.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from queue import Queue
 from pathlib import Path
 import time
+import shutil
 
 from modules import Logger
 from modules.info import ProjectData
@@ -26,29 +27,45 @@ from customtkinter import StringVar
 
 def run(mode: Literal["Player", "Studio"], textvariable: StringVar, versioninfovariable: StringVar, end_signal: Callable, exception_queue: Queue) -> None:
     try:
+        # Get deployment info
         Logger.info("Getting deployment info...")
+        textvariable.set("Getting deployment info...")
         deployment: Deployment = Deployment(mode)
         if settings.get_value("show_deployment_info_on_launch"):
             versioninfovariable.set(f"{deployment.version} ({deployment.channel})")
 
+        # Forced Roblox reinstallation
+        if settings.get_value("force_roblox_reinstallation"):
+            Logger.debug("Forced Roblox reinstallation")
+            textvariable.set("Forced Roblox reinstallation")
+            settings.set_value("force_roblox_reinstallation", False)
+            shutil.rmtree(Directory.DOWNLOADS / mode, ignore_errors=True)
+
+        # Check for downloaded files
         Logger.info("Checking Downloads folder...")
+        textvariable.set("Checking downloaded files...")
         missing_file_hashes: list[str] = check_downloaded_files(deployment, mode)
 
+        # Download missing files
         if missing_file_hashes:
             Logger.info("Downloading missing files...")
             textvariable.set(f"Downloading Roblox {mode}...")
             download_missing_files(deployment, mode, missing_file_hashes)
 
+        # Check if Roblox is already running
         if process_exists(deployment.executable_name):
             if settings.get_value("confirm_launch_if_roblox_running"):
                 if not messagebox.askyesno(ProjectData.NAME, "Another Roblox instance is already running!\nDo you still wish to continue?"):
                     return
             kill_process(deployment.executable_name)
         
-        Logger.info("Restoring default files...")
-        textvariable.set(f"Installing Roblox {mode}...")
-        restore_default_files(deployment, mode)
+        # Restore default files, if needed
+        if settings.get_value("restore_default_files") or missing_file_hashes:
+            Logger.info("Restoring default files...")
+            textvariable.set(f"Installing Roblox {mode}...")
+            restore_default_files(deployment, mode)
 
+        # Update mods, if needed
         if integrations.get_value("mod_updater"):
             active_mods: list[str] = mods.get_active(mode)
             check: dict[str, list[Path]] | Literal[False] = check_for_mod_updates(Directory.MODS, active_mods, deployment.version)
@@ -57,6 +74,7 @@ def run(mode: Literal["Player", "Studio"], textvariable: StringVar, versioninfov
                 textvariable.set("Updating mods...")
                 update_mods(check, deployment.version, Directory.MODS)
 
+        # Apply modifications
         disable_all_mods: bool = settings.get_value("disable_all_mods")
         disable_all_fastflags: bool = settings.get_value("disable_all_fastflags")
         if not disable_all_mods or not disable_all_fastflags:
@@ -67,14 +85,16 @@ def run(mode: Literal["Player", "Studio"], textvariable: StringVar, versioninfov
         if not disable_all_fastflags:
             apply_fastflags(deployment.base_directory, mode)
 
+        # Launch Roblox
         Logger.info(f"Launching Roblox {mode}...")
         textvariable.set(f"Launching Roblox {mode}...")
         launch_roblox(str(deployment.executable_path.resolve()))
 
+        # Start launch apps
         Logger.info("Starting launch apps...")
         start_launch_apps(mode)
 
-        # It usually takes a second or two for Roblox to appear
+        # It takes a second or two for Roblox to appear after it's launched
         time.sleep(2)
     
     except Exception as e:
--- modules/launcher/tasks/check_downloaded_files.py
+++ modules/launcher/tasks/check_downloaded_files.py
@@ -1,10 +1,8 @@
 from typing import Literal
 from pathlib import Path
 import os
-import shutil
 
 from modules.filesystem import Directory
-from modules.config import settings
 
 from ..deployment_info import Deployment
 
@@ -12,10 +10,6 @@ from ..deployment_info import Deployment
 def check_downloaded_files(deployment: Deployment, mode: Literal["Player", "Studio"]) -> list[str]:
     directory: Path = Directory.DOWNLOADS / mode
     directory.mkdir(parents=True, exist_ok=True)
-
-    if settings.get_value("force_roblox_reinstallation"):
-        settings.set_value("force_roblox_reinstallation", False)
-        shutil.rmtree(directory, ignore_errors=True)
 
     required_file_hashes: list[str] = [
         item["hash"]
