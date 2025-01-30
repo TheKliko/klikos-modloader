4e81ec88215d5de1b2bb2560e08a2c1976204246
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-01-30 13:46:54 +0100

    Add GlobalBasicSettings editor
    And now it's functional :D

--- modules/menu/sections/global_basic_settings_editor.py
+++ modules/menu/sections/global_basic_settings_editor.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from xml.etree import ElementTree as xml
+from typing import Any
+from tkinter.messagebox import showwarning
 
+from modules import Logger
+from modules.info import ProjectData
 from modules.filesystem import File, Directory, restore_from_meipass
 from modules.functions.interface.image import load as load_image
 
@@ -10,7 +14,7 @@ import customtkinter as ctk
 class GlobalBasicSettingsSection:
     class Constants:
         SECTION_TITLE: str = "GlobalBasicSettings Editor"
-        SECTION_DESCRIPTION: str = "Change basic settings (Does not affect Roblox Studio)"
+        SECTION_DESCRIPTION: str = "Don't change these settings unless you know what you're doing"
         ENTRY_INNER_PADDING: int = 4
         ENTRY_OUTER_PADDING: int = 12
         ENTRY_GAP: int = 8
@@ -60,7 +64,7 @@ class GlobalBasicSettingsSection:
     # region content
     def _load_content(self) -> None:
         container: ctk.CTkFrame = ctk.CTkFrame(self.container, fg_color="transparent")
-        container.grid_columnconfigure(0, weight=1)
+        container.grid_columnconfigure((0, 1), weight=1)
         container.grid(column=0, row=1, sticky="nsew", padx=(0,4))
 
         if not File.GLOBAL_BASIC_SETTINGS.is_file():
@@ -101,13 +105,14 @@ class GlobalBasicSettingsSection:
             if "name" not in element.attrib:
                 continue
             
-            frame: ctk.CTkFrame = ctk.CTkFrame(container)
+            frame: ctk.CTkFrame = ctk.CTkFrame(container, width=(container.winfo_width()//2)-self.Constants.ENTRY_GAP)
             frame.grid_columnconfigure(0, weight=1)
+            frame.grid_propagate(True)
             frame.grid(column=self._get_frame_column(i), row=self._get_frame_row(i), sticky="nsew", pady=self._get_frame_pady(i), padx=self._get_frame_padx(i))
             i += 1
 
             # Name label
-            ctk.CTkLabel(frame, text=f"[{element.tag}] {element.attrib['name']}", anchor="w", font=self.Fonts.bold).grid(column=0, row=0, sticky="nw", padx=self.Constants.ENTRY_OUTER_PADDING, pady=(self.Constants.ENTRY_OUTER_PADDING, self.Constants.ENTRY_INNER_PADDING))
+            ctk.CTkLabel(frame, text=f"[{element.tag.upper()}] {element.attrib['name']}", anchor="w", font=self.Fonts.bold).grid(column=0, row=0, sticky="nw", padx=self.Constants.ENTRY_OUTER_PADDING, pady=(self.Constants.ENTRY_OUTER_PADDING, self.Constants.ENTRY_INNER_PADDING))
 
             # Content
             content_frame: ctk.CTkFrame = ctk.CTkFrame(frame, fg_color="transparent")
@@ -136,7 +141,7 @@ class GlobalBasicSettingsSection:
                     x_entry.insert("end", x_value)
                     x_entry.bind("<Return>", lambda _: self.root.focus())
                     x_entry.bind("<Control-s>", lambda _: self.root.focus())
-                    # entry.bind("<FocusOut>", lambda event: ???)
+                    x_entry.bind("<FocusOut>", lambda event, name=element.attrib['name']: self._update_vector2_x_value(tree=tree, name=name, event=event))
                     x_entry.grid(column=1, row=0, sticky="ew", padx=(0, self.Constants.ENTRY_INNER_PADDING))
 
                     ctk.CTkLabel(content_frame, text="Y: ", anchor="e").grid(column=2, row=0, sticky="nsew")
@@ -147,20 +152,20 @@ class GlobalBasicSettingsSection:
                     y_entry.insert("end", y_value)
                     y_entry.bind("<Return>", lambda _: self.root.focus())
                     y_entry.bind("<Control-s>", lambda _: self.root.focus())
-                    # entry.bind("<FocusOut>", lambda event: ???)
+                    y_entry.bind("<FocusOut>", lambda event, name=element.attrib['name']: self._update_vector2_y_value(tree=tree, name=name, event=event))
                     y_entry.grid(column=3, row=0, sticky="ew")
 
                 # region bool
                 case "bool":
-                    value = element.text or "False"
+                    value = element.text or "false"
                     value = value.strip()
-                    value = bool(value)
+                    value = value == "true"
 
                     ctk.CTkLabel(content_frame, text="Value: ", anchor="e").grid(column=0, row=0, sticky="nsew")
                     var: ctk.BooleanVar = ctk.BooleanVar(value=value)
                     ctk.CTkSwitch(
                         content_frame, text="", width=48, variable=var, onvalue=True, offvalue=False,
-                        command=lambda var=var: print(var.get())
+                        command=lambda var=var, name=element.attrib['name']: self._update_bool_value(tree=tree, name=name, value=var.get())
                     ).grid(column=1, row=0, sticky="e", padx=(8, 0))
                 
                 # region int
@@ -178,7 +183,7 @@ class GlobalBasicSettingsSection:
                     entry.insert("end", value)
                     entry.bind("<Return>", lambda _: self.root.focus())
                     entry.bind("<Control-s>", lambda _: self.root.focus())
-                    # entry.bind("<FocusOut>", lambda event: ???)
+                    entry.bind("<FocusOut>", lambda event, name=element.attrib['name']: self._update_int_value(tree=tree, name=name, event=event))
                     entry.grid(column=1, row=0, sticky="ew", padx=(0, self.Constants.ENTRY_INNER_PADDING))
                 
                 # region float
@@ -196,7 +201,7 @@ class GlobalBasicSettingsSection:
                     entry.insert("end", value)
                     entry.bind("<Return>", lambda _: self.root.focus())
                     entry.bind("<Control-s>", lambda _: self.root.focus())
-                    # entry.bind("<FocusOut>", lambda event: ???)
+                    entry.bind("<FocusOut>", lambda event, name=element.attrib['name']: self._update_float_value(tree=tree, name=name, event=event))
                     entry.grid(column=1, row=0, sticky="ew", padx=(0, self.Constants.ENTRY_INNER_PADDING))
                 
                 # region string
@@ -213,7 +218,7 @@ class GlobalBasicSettingsSection:
                     entry.insert("end", value)
                     entry.bind("<Return>", lambda _: self.root.focus())
                     entry.bind("<Control-s>", lambda _: self.root.focus())
-                    # entry.bind("<FocusOut>", lambda event: ???)
+                    entry.bind("<FocusOut>", lambda event, name=element.attrib['name']: self._update_string_value(tree=tree, name=name, event=event))
                     entry.grid(column=1, row=0, sticky="ew", padx=(0, self.Constants.ENTRY_INNER_PADDING))
                 
                 # region token
@@ -230,42 +235,21 @@ class GlobalBasicSettingsSection:
                     entry.insert("end", value)
                     entry.bind("<Return>", lambda _: self.root.focus())
                     entry.bind("<Control-s>", lambda _: self.root.focus())
-                    # entry.bind("<FocusOut>", lambda event: ???)
+                    entry.bind("<FocusOut>", lambda event, name=element.attrib['name']: self._update_token_value(tree=tree, name=name, event=event))
                     entry.grid(column=1, row=0, sticky="ew", padx=(0, self.Constants.ENTRY_INNER_PADDING))
-
-
-            # TODO: Implement functionality
-            # TODO: remove this later
-            name: str = element.attrib["name"]
-            print(f"Name: {name} (Tag: {element.tag})")
             
-            if element.tag == "Vector2":
-                    x = element.find("X")
-                    if x is None:
-                        x = "N/A"
-                    else:
-                        x = "N/A" if x.text is None else x.text.strip()
-                        
-                    y = element.find("Y")
-                    if y is None:
-                        y = "N/A"
-                    else:
-                        y = "N/A" if y.text is None else y.text.strip()
-                    
-                    print(f"X: {x}")
-                    print(f"Y: {y}")
-            
-            else:
-                text = element.text or ""
-                text = text.strip()
-                print(f"Value: {text}")
-
+            # Height is hardcoded for now because it's way faster and it doesn't give any big issues
+            # frame.update_idletasks()
+            # frame_height: int = frame.winfo_height()
+            frame_height: int = 96
+            frame.grid_propagate(False)
+            frame.configure(width=(container.winfo_width()//2)-self.Constants.ENTRY_GAP, height=frame_height)
     # endregion
 
 
     # region functions
     def _get_frame_column(self, i) -> int:
-        return 1 - (i % 2)
+        return i % 2
     
     def _get_frame_row(self, i) -> int:
         return i // 2
@@ -276,5 +260,95 @@ class GlobalBasicSettingsSection:
     def _get_frame_pady(self, i) -> int | tuple[int, int]:
         return (self.Constants.ENTRY_GAP, 0) if self._get_frame_row(i) >= 1 else 0
     
-    # TODO: Implement functionality
+    def _update_bool_value(self, tree: xml.ElementTree, name: str, value: bool) -> None:
+        new_value: str = str(value).lower()
+        element = tree.find(f".//*[@name='{name}']")
+        if element is None:
+            Logger.error(f"Failed to update value for {name}!\nElement not found!", prefix="GlobalBasicSettingsSection._update_boolean_value()")
+            showwarning(ProjectData.NAME, f"Failed to update value for {name}!\nElement not found!")
+            return
+
+        element.text = new_value
+        tree.write(File.GLOBAL_BASIC_SETTINGS)
+    
+    def _update_int_value(self, tree: xml.ElementTree, name: str, event) -> None:
+        new_value: str = str(event.widget.get())
+
+        if not new_value:
+            new_value = "0"
+            event.widget.delete(0, "end")
+            event.widget.insert(0, "0")
+        
+        element = tree.find(f".//*[@name='{name}']")
+        if element is None:
+            Logger.error(f"Failed to update value for {name}!\nElement not found!", prefix="GlobalBasicSettingsSection._update_boolean_value()")
+            showwarning(ProjectData.NAME, f"Failed to update value for {name}!\nElement not found!")
+            return
+
+        element.text = new_value
+        tree.write(File.GLOBAL_BASIC_SETTINGS)
+    
+    def _update_float_value(self, tree: xml.ElementTree, name: str, event) -> None:
+        self._update_int_value(tree=tree, name=name, event=event)
+    
+    def _update_string_value(self, tree: xml.ElementTree, name: str, event) -> None:
+        new_value: str = str(event.widget.get())
+        
+        element = tree.find(f".//*[@name='{name}']")
+        if element is None:
+            Logger.error(f"Failed to update value for {name}!\nElement not found!", prefix="GlobalBasicSettingsSection._update_boolean_value()")
+            showwarning(ProjectData.NAME, f"Failed to update value for {name}!\nElement not found!")
+            return
+
+        element.text = new_value
+        tree.write(File.GLOBAL_BASIC_SETTINGS)
+    
+    def _update_token_value(self, tree: xml.ElementTree, name: str, event) -> None:
+        self._update_string_value(tree=tree, name=name, event=event)
+    
+    def _update_vector2_x_value(self, tree: xml.ElementTree, name: str, event) -> None:
+        new_value: str = str(event.widget.get())
+
+        if not new_value:
+            new_value = "0"
+            event.widget.delete(0, "end")
+            event.widget.insert(0, "0")
+        
+        element = tree.find(f".//*[@name='{name}']")
+        if element is None:
+            Logger.error(f"Failed to update value for {name}!\nElement not found!", prefix="GlobalBasicSettingsSection._update_boolean_value()")
+            showwarning(ProjectData.NAME, f"Failed to update value for {name}!\nElement not found!")
+            return
+        
+        x_element = tree.find(f".//*[@name='{name}']")
+        if x_element is None:
+            Logger.error(f"Failed to update value for {name}!\nX element not found!", prefix="GlobalBasicSettingsSection._update_boolean_value()")
+            showwarning(ProjectData.NAME, f"Failed to update value for {name}!\nX element not found!")
+            return
+
+        x_element.text = new_value
+        tree.write(File.GLOBAL_BASIC_SETTINGS)
+    
+    def _update_vector2_y_value(self, tree: xml.ElementTree, name: str, event) -> None:
+        new_value: str = str(event.widget.get())
+
+        if not new_value:
+            new_value = "0"
+            event.widget.delete(0, "end")
+            event.widget.insert(0, "0")
+        
+        element = tree.find(f".//*[@name='{name}']")
+        if element is None:
+            Logger.error(f"Failed to update value for {name}!\nElement not found!", prefix="GlobalBasicSettingsSection._update_boolean_value()")
+            showwarning(ProjectData.NAME, f"Failed to update value for {name}!\nElement not found!")
+            return
+        
+        y_element = tree.find(f".//*[@name='{name}']")
+        if y_element is None:
+            Logger.error(f"Failed to update value for {name}!\nY element not found!", prefix="GlobalBasicSettingsSection._update_boolean_value()")
+            showwarning(ProjectData.NAME, f"Failed to update value for {name}!\nY element not found!")
+            return
+
+        y_element.text = new_value
+        tree.write(File.GLOBAL_BASIC_SETTINGS)
     # endregion
