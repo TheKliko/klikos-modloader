7954f28dabeff543e4515a43002351a6e5fcc24b
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-02-13 16:41:16 +0100

    Update create_singleton_mutex.py
    - Added ROBLOX_singletonEvent

--- modules/launcher/tasks/create_singleton_mutex.py
+++ modules/launcher/tasks/create_singleton_mutex.py
@@ -1,21 +1,21 @@
 import ctypes
-import sys
 
 from modules import Logger
 
 
 def create_singleton_mutex() -> None:
     kernel32 = ctypes.windll.kernel32
-    mutex_name: str = "ROBLOX_singletonMutex"
+    mutexes: list[str] = ["ROBLOX_singletonMutex", "ROBLOX_singletonEvent"]
 
-    Logger.info(f"Creating mutex: {mutex_name}", prefix="launcher.tasks.create_singleton_mutex()")
-    mutex = kernel32.CreateMutexW(None, False, mutex_name)
+    for mutex_name in mutexes:
+        Logger.info(f"Creating mutex: {mutex_name}", prefix="launcher.tasks.create_singleton_mutex()")
+        mutex = kernel32.CreateMutexW(None, False, mutex_name)
 
-    if mutex == 0:
-        Logger.warning(f"Failed to create mutex: {mutex_name}. {ctypes.WinError()}", prefix="launcher.tasks.create_singleton_mutex()")
-        return
-    
-    if kernel32.GetLastError() == 193:
-        Logger.warning(f"Mutex already exists: {mutex_name}", prefix="launcher.tasks.create_singleton_mutex()")
-    else:
-        Logger.info("Mutex created successfully!", prefix="launcher.tasks.create_singleton_mutex()")
+        if mutex == 0:
+            Logger.warning(f"Failed to create mutex: {mutex_name}. {ctypes.WinError()}", prefix="launcher.tasks.create_singleton_mutex()")
+            return
+        
+        if kernel32.GetLastError() == 193:
+            Logger.warning(f"Mutex already exists: {mutex_name}", prefix="launcher.tasks.create_singleton_mutex()")
+        else:
+            Logger.info("Mutex created successfully!", prefix="launcher.tasks.create_singleton_mutex()")
