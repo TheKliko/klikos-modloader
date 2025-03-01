cac00b35ae7cdffd8f634c3a08e9b741f6c56b34
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-03-01 19:45:06 +0100

    Delete js directory

--- js/mobile.js
+++
@@ -1,8 +0,0 @@
-function toggle_navigation() {
-    var navigation = document.getElementById("header-navigation");
-    if (navigation.classList.contains("active")) {
-        navigation.classList.remove("active");
-    } else {
-        navigation.classList.add("active");
-    }
-}
